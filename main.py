850844ade832275747af1dcd3629ba6cf67d47fb
ZA - 11TM 702210 - <120271066+ZA-702210@users.noreply.github.com>
2023-01-20 02:48:27 +0000

    Added new file, clear notepad, save file

--- main.py
+++ main.py
@@ -2,14 +2,6 @@ from tkinter import *
 from tkinter import filedialog
 import os
 
-TextFiles = os.listdir('TBN_Bin/TextFiles/')
-
-getTxtFiles = []
-for i in TextFiles:
-  fileName, fileExtension = os.path.splitext(i)
-  if fileExtension == ".txt":
-    getTxtFiles.append(i)
-
 
 root = Tk()
 root.geometry("500x250")
@@ -21,26 +13,46 @@ root.columnconfigure(1,weight=1)
 root.columnconfigure(2,weight=1)
 root.columnconfigure(3,weight=1)
 
+
 def openFile():
-  global file
-  root.filename = filedialog.askopenfilename(initialdir="/TBN_Bin/", title="Select a TEXT file", filetypes=(("text files","*.txt"),("all files", "*.*")))
+  root.filename = filedialog.askopenfilename(title="Select a TEXT file", filetypes=(("text files","*.txt"),("all files", "*.*")))
   selectedTextFile = open(root.filename, 'r')
+  print(root.filename)
   print(selectedTextFile.read())
 #
 
+
 def saveFile():
+  notepadText = textBox.get('1.0','end')
+  root.filename = filedialog.asksaveasfilename(initialdir="/home/runner/typewriter/TBN_Bin/TextFiles",title='Choose a location to save',filetypes=(("text files","*.txt"),("all files","*.*")))
+  createNewFile = open(root.filename, 'w')
+  createNewFile.write(notepadText)
+  createNewFile.close()
   
-    
+#
+
+
+def newFile():
+  root.filename = filedialog.asksaveasfilename(initialdir="/home/runner/typewriter/TBN_Bin/TextFiles",title='Choose a location to save',filetypes=(("text files","*.txt"),("all files","*.*")))
+  createNewFile = open(root.filename, 'w')
+  createNewFile.close()
+  textBox.delete('1.0', END)
+
+#
+
+
+def clearNotepad():
+  textBox.delete('1.0', END)
+#
   
 openFileButton = Button(root,text="Open File",command=openFile).grid(row=0,column=0,padx=1,pady=1)
-saveFileButton = Button(root,text="Save File").grid(row=0,column=1,padx=1,pady=1)
-newFileButton = Button(root,text="New File").grid(row=0,column=2,padx=1,pady=1)
-settingsButton = Button(root,text="Settings").grid(row=0,column=3,padx=1,pady=1)
+saveFileButton = Button(root,text="Save File",command=saveFile).grid(row=0,column=1,padx=1,pady=1)
+newFileButton = Button(root,text="New File",command=newFile).grid(row=0,column=2,padx=1,pady=1)
+clearNotepadButton = Button(root,text="Clear Notepad",command=clearNotepad).grid(row=0,column=3,padx=1,pady=1)
+
+
 textBox = Text(root)
-  
-
 textBox.grid(row=1,column=0,columnspan=4)
-textBox.insert('1.0',"hi")
-
+textBox.insert('1.0',"Placeholder text")
 
 root.mainloop()
